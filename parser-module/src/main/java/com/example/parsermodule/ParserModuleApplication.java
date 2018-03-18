8a6350aab2f14d58db1323e627fbdb695a3e651f
Viet <vietsemantics@gmail.com>
2018-03-18 17:16:06 -0500

    add module

---
+++ parser-module/src/main/java/com/example/parsermodule/ParserModuleApplication.java
@@ -0,0 +1,12 @@
+package com.example.parsermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParserModuleApplication.class, args);
+	}
+}
